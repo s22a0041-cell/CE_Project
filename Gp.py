ff6acc1b5de8658cb86286fd8a34d168593797be
s22a0041-cell <s22a0041@siswa.umk.edu.my>
2026-01-12 21:08:44 +0800

    Update Gp.py

--- Gp.py
+++ Gp.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import random
-import operator
 import numpy as np
 import pandas as pd
-
-from deap import base, creator, gp, tools, algorithms
 
 # -------------------------------------------------
 # STREAMLIT CONFIG
@@ -14,7 +11,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Multi-objective Job Scheduling using Genetic Programming")
+st.title("Job Scheduling using Genetic Programming")
 st.write("Genetic Programming to evolve scheduling rules on limited machines")
 
 # -------------------------------------------------
@@ -24,8 +21,9 @@ st.sidebar.header("Simulation Parameters")
 
 NUM_JOBS = st.sidebar.slider("Number of Jobs", 3, 10, 5)
 NUM_MACHINES = st.sidebar.slider("Number of Machines", 1, 3, 2)
-POP_SIZE = st.sidebar.slider("Population Size", 50, 300, 100)
-GENS = st.sidebar.slider("Generations", 10, 100, 30)
+POP_SIZE = st.sidebar.slider("Population Size", 20, 200, 50)
+GENERATIONS = st.sidebar.slider("Generations", 10, 100, 30)
+MUTATION_RATE = st.sidebar.slider("Mutation Rate", 0.0, 1.0, 0.2)
 
 random.seed(42)
 
@@ -33,11 +31,11 @@ random.seed(42)
 # JOB DATA
 # -------------------------------------------------
 jobs = []
-for j in range(NUM_JOBS):
+for i in range(NUM_JOBS):
     jobs.append({
-        "job_id": f"J{j+1}",
-        "processing_time": random.randint(1, 10),
-        "release_time": random.randint(0, 5)
+        "job": f"J{i+1}",
+        "processing": random.randint(1, 10),
+        "release": random.randint(0, 5)
     })
 
 jobs_df = pd.DataFrame(jobs)
@@ -46,123 +44,114 @@ st.subheader("Job Data")
 st.dataframe(jobs_df, use_container_width=True)
 
 # -------------------------------------------------
-# GP PRIMITIVES
+# GP REPRESENTATION (RULE TREE AS EXPRESSION STRING)
 # -------------------------------------------------
-pset = gp.PrimitiveSet("MAIN", 2)
-pset.renameArguments(ARG0="p_time", ARG1="wait_time")
+OPERATORS = ["+", "-", "*"]
+TERMINALS = ["p", "w", "1", "2"]
 
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(min, 2)
-pset.addPrimitive(max, 2)
+def random_expression(depth=2):
+    if depth == 0:
+        return random.choice(TERMINALS)
+    return f"({random_expression(depth-1)} {random.choice(OPERATORS)} {random_expression(depth-1)})"
 
-pset.addEphemeralConstant("rand", lambda: random.uniform(-1, 1))
-
-# -------------------------------------------------
-# FITNESS & INDIVIDUAL
-# -------------------------------------------------
-if "FitnessMin" not in creator.__dict__:
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-if "Individual" not in creator.__dict__:
-    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
-
-toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=3)
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("compile", gp.compile, pset=pset)
+def evaluate_expression(expr, p, w):
+    try:
+        return eval(expr, {"p": p, "w": w})
+    except:
+        return float("inf")
 
 # -------------------------------------------------
 # SCHEDULING SIMULATION
 # -------------------------------------------------
-def simulate_schedule(rule_func):
+def simulate(rule_expr):
     time = 0
-    machine_available = [0] * NUM_MACHINES
+    machines = [0] * NUM_MACHINES
     completion_times = []
-
     job_queue = jobs.copy()
 
     while job_queue:
-        available_jobs = [j for j in job_queue if j["release_time"] <= time]
+        available = [j for j in job_queue if j["release"] <= time]
 
-        if not available_jobs:
+        if not available:
             time += 1
             continue
 
-        scored_jobs = []
-        for job in available_jobs:
-            wait_time = time - job["release_time"]
-            score = rule_func(job["processing_time"], wait_time)
-            scored_jobs.append((score, job))
+        scores = []
+        for j in available:
+            wait = time - j["release"]
+            score = evaluate_expression(rule_expr, j["processing"], wait)
+            scores.append((score, j))
 
-        scored_jobs.sort(key=lambda x: x[0])
-        selected_job = scored_jobs[0][1]
+        scores.sort(key=lambda x: x[0])
+        selected = scores[0][1]
 
-        machine_index = machine_available.index(min(machine_available))
-        start_time = max(time, machine_available[machine_index])
-        finish_time = start_time + selected_job["processing_time"]
+        m = machines.index(min(machines))
+        start = max(time, machines[m])
+        finish = start + selected["processing"]
 
-        machine_available[machine_index] = finish_time
-        completion_times.append(finish_time)
-
-        job_queue.remove(selected_job)
+        machines[m] = finish
+        completion_times.append(finish)
+        job_queue.remove(selected)
         time += 1
 
     makespan = max(completion_times)
     total_completion = sum(completion_times)
-    idle_time = sum(machine_available) - makespan
+    idle_time = sum(machines) - makespan
 
     fitness = makespan + 0.1 * total_completion + 0.1 * idle_time
     return fitness
 
 # -------------------------------------------------
-# FITNESS FUNCTION
+# GENETIC OPERATORS
 # -------------------------------------------------
-def eval_individual(individual):
-    func = toolbox.compile(expr=individual)
-    return (simulate_schedule(func),)
+def crossover(p1, p2):
+    cut1 = random.randint(1, len(p1)-2)
+    cut2 = random.randint(1, len(p2)-2)
+    return p1[:cut1] + p2[cut2:]
 
-toolbox.register("evaluate", eval_individual)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", gp.cxOnePoint)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr, pset=pset)
+def mutate(expr):
+    if random.random() < MUTATION_RATE:
+        return random_expression(2)
+    return expr
 
-toolbox.decorate("mate", gp.staticLimit(key=len, max_value=10))
-toolbox.decorate("mutate", gp.staticLimit(key=len, max_value=10))
+def tournament_selection(pop, fitness, k=3):
+    selected = random.sample(list(zip(pop, fitness)), k)
+    selected.sort(key=lambda x: x[1])
+    return selected[0][0]
 
 # -------------------------------------------------
 # RUN GP
 # -------------------------------------------------
 if st.button("Run Genetic Programming"):
-    pop = toolbox.population(n=POP_SIZE)
-    hof = tools.HallOfFame(1)
+    population = [random_expression(2) for _ in range(POP_SIZE)]
+    best_fitness = []
+    best_rule = None
 
-    stats = tools.Statistics(lambda ind: ind.fitness.values[0])
-    stats.register("avg", np.mean)
-    stats.register("min", np.min)
+    for gen in range(GENERATIONS):
+        fitness_values = [simulate(ind) for ind in population]
+        best_idx = np.argmin(fitness_values)
 
-    pop, logbook = algorithms.eaSimple(
-        pop,
-        toolbox,
-        cxpb=0.5,
-        mutpb=0.2,
-        ngen=GENS,
-        stats=stats,
-        halloffame=hof,
-        verbose=False
-    )
+        best_fitness.append(fitness_values[best_idx])
+        best_rule = population[best_idx]
 
-    log_df = pd.DataFrame(logbook)
+        new_population = []
+        for _ in range(POP_SIZE):
+            parent1 = tournament_selection(population, fitness_values)
+            parent2 = tournament_selection(population, fitness_values)
+            child = crossover(parent1, parent2)
+            child = mutate(child)
+            new_population.append(child)
+
+        population = new_population
 
     st.subheader("Fitness Convergence")
     st.line_chart(
-        log_df.set_index("gen")[["min"]],
+        pd.DataFrame({"Fitness": best_fitness}),
         use_container_width=True
     )
 
-    st.subheader("Best Evolved Scheduling Rule")
-    st.code(str(hof[0]), language="text")
+    st.subheader("Best Scheduling Rule (Evolved)")
+    st.code(best_rule, language="text")
 
     st.subheader("Best Fitness Value")
-    st.write(hof[0].fitness.values[0])
+    st.write(best_fitness[-1])
