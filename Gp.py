051a615456acc949c8735b2d375042d8d8a22d77
s22a0041-cell <s22a0041@siswa.umk.edu.my>
2026-01-12 21:05:37 +0800

    Update Gp.py

--- Gp.py
+++ Gp.py
@@ -3,24 +3,23 @@ import random
 import operator
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from deap import base, creator, gp, tools, algorithms
 
-# -----------------------------
+# -------------------------------------------------
 # STREAMLIT CONFIG
-# -----------------------------
+# -------------------------------------------------
 st.set_page_config(
     page_title="GP Job Scheduling Optimization",
     layout="wide"
 )
 
 st.title("Multi-objective Job Scheduling using Genetic Programming")
-st.write("Genetic Programming to evolve scheduling rules for limited machines")
+st.write("Genetic Programming to evolve scheduling rules on limited machines")
 
-# -----------------------------
-# SIDEBAR INPUT
-# -----------------------------
+# -------------------------------------------------
+# SIDEBAR PARAMETERS
+# -------------------------------------------------
 st.sidebar.header("Simulation Parameters")
 
 NUM_JOBS = st.sidebar.slider("Number of Jobs", 3, 10, 5)
@@ -30,9 +29,9 @@ GENS = st.sidebar.slider("Generations", 10, 100, 30)
 
 random.seed(42)
 
-# -----------------------------
+# -------------------------------------------------
 # JOB DATA
-# -----------------------------
+# -------------------------------------------------
 jobs = []
 for j in range(NUM_JOBS):
     jobs.append({
@@ -46,25 +45,27 @@ jobs_df = pd.DataFrame(jobs)
 st.subheader("Job Data")
 st.dataframe(jobs_df, use_container_width=True)
 
-# -----------------------------
+# -------------------------------------------------
 # GP PRIMITIVES
-# -----------------------------
+# -------------------------------------------------
 pset = gp.PrimitiveSet("MAIN", 2)
 pset.renameArguments(ARG0="p_time", ARG1="wait_time")
 
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(max, 2)
 pset.addPrimitive(min, 2)
+pset.addPrimitive(max, 2)
 
 pset.addEphemeralConstant("rand", lambda: random.uniform(-1, 1))
 
-# -----------------------------
+# -------------------------------------------------
 # FITNESS & INDIVIDUAL
-# -----------------------------
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+# -------------------------------------------------
+if "FitnessMin" not in creator.__dict__:
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+if "Individual" not in creator.__dict__:
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=3)
@@ -72,9 +73,9 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-# -----------------------------
+# -------------------------------------------------
 # SCHEDULING SIMULATION
-# -----------------------------
+# -------------------------------------------------
 def simulate_schedule(rule_func):
     time = 0
     machine_available = [0] * NUM_MACHINES
@@ -109,18 +110,18 @@ def simulate_schedule(rule_func):
         time += 1
 
     makespan = max(completion_times)
-    total_waiting = sum(completion_times)
+    total_completion = sum(completion_times)
     idle_time = sum(machine_available) - makespan
 
-    return makespan + 0.1 * total_waiting + 0.1 * idle_time
+    fitness = makespan + 0.1 * total_completion + 0.1 * idle_time
+    return fitness
 
-# -----------------------------
+# -------------------------------------------------
 # FITNESS FUNCTION
-# -----------------------------
+# -------------------------------------------------
 def eval_individual(individual):
     func = toolbox.compile(expr=individual)
-    fitness = simulate_schedule(func)
-    return (fitness,)
+    return (simulate_schedule(func),)
 
 toolbox.register("evaluate", eval_individual)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -130,18 +131,20 @@ toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr, pset=pset)
 toolbox.decorate("mate", gp.staticLimit(key=len, max_value=10))
 toolbox.decorate("mutate", gp.staticLimit(key=len, max_value=10))
 
-# -----------------------------
+# -------------------------------------------------
 # RUN GP
-# -----------------------------
+# -------------------------------------------------
 if st.button("Run Genetic Programming"):
     pop = toolbox.population(n=POP_SIZE)
     hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values[0])
     stats.register("avg", np.mean)
     stats.register("min", np.min)
 
-    pop, result_log = algorithms.eaSimple(
-        pop, toolbox,
+    pop, logbook = algorithms.eaSimple(
+        pop,
+        toolbox,
         cxpb=0.5,
         mutpb=0.2,
         ngen=GENS,
@@ -150,17 +153,15 @@ if st.button("Run Genetic Programming"):
         verbose=False
     )
 
-    log_df = pd.DataFrame(result_log)
-    st.subheader("Convergence Graph")
+    log_df = pd.DataFrame(logbook)
 
-    fig, ax = plt.subplots()
-    ax.plot(log_df["gen"], log_df["min"], label="Best Fitness")
-    ax.set_xlabel("Generation")
-    ax.set_ylabel("Fitness")
-    ax.legend()
-    st.pyplot(fig, use_container_width=True)
+    st.subheader("Fitness Convergence")
+    st.line_chart(
+        log_df.set_index("gen")[["min"]],
+        use_container_width=True
+    )
 
-    st.subheader("Best Scheduling Rule (GP Tree)")
+    st.subheader("Best Evolved Scheduling Rule")
     st.code(str(hof[0]), language="text")
 
     st.subheader("Best Fitness Value")
